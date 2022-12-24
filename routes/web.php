c0be4b26996157fb8b04ddd122da55884bc8cb81
diahnkha <diahnurkhasanah5@gmail.com>
2022-12-24 09:53:37 +0700

    add endpoint for api admin and aspirasi

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/list', [AdminController::class, 'index']);
+Route::get('/admin/{id}/show', [AdminController::class, 'show']);
+Route::post('/admin/store', [AdminController::class, 'store']);
+Route::post('/admin/{id}/update', [AdminController::class, 'update']);
+Route::post('/admin/{id}/delete', [AdminController::class, 'destroy']);
+
+Route::get('/aspirasi/list', [AspirasiController::class, 'index']);
+Route::get('/aspirasi/{id}/show', [AspirasiController::class, 'show']);
+Route::post('/aspirasi/store', [AspirasiController::class, 'store']);
+Route::post('/aspirasi/{id}/update', [AspirasiController::class, 'update']);
+Route::post('/aspirasi/{id}/delete', [AspirasiController::class, 'destroy']);
