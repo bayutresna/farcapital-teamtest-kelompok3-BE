e99fd0fe60b6f28d56b8d1be3c56ae0d459a2977
diahnkha <diahnurkhasanah5@gmail.com>
2022-12-24 09:36:53 +0700

    add migration table user and aspirasi

---
+++ database/migrations/2022_12_24_023131_create_aspirasi_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aspirasi', function (Blueprint $table) {
+            $table->id();
+            $table->string("nama", 50);
+            $table->text("cerita");
+            $table->text("foto");
+            $table->string("judul",100);
+            $table->string("email");
+            $table->tinyinteger("is_anonim");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aspirasi');
+    }
+};
