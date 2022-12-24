f798646f5de0a636b624528c8907312fe170db75
Bayu Tresna <bayutresna2000@gmail.com>
2022-12-24 10:05:01 +0700

    add hash on boot

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,4 +9,19 @@ class User extends Model
 {
     use HasFactory;
     public $guarded = ['id'];
+    protected $table = 'admin';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function (User $admin) {
+            $admin->password = Hash::make($admin->password);
+        });
+        static::updating(function (User $admin) {
+            if ($admin->isDirty(["password"])) {
+                $admin->password = Hash::make($admin->password);
+            }
+        });
+    }
 }
